71a8b22f0287f567557a6bf182f2b1303467a4a5
Marcelo Pereira <132702644+MarjoDev@users.noreply.github.com>
2024-08-19 10:47:22 -0300

    Adição de objetos e funções

--- Lista-2/src/main/java/Main.java
+++ Lista-2/src/main/java/Main.java
@@ -6,5 +6,16 @@ public class Main {
 
         Aluno obj2 = new Aluno();
         obj2.setNome("Fei");
+        obj2.setRa(123);
+        obj2.setMedia(7);
+
+        System.out.println("RA: " + obj2.getRa());
+
+        Aluno obj3 = new Aluno();
+        obj3.setNome("Pelé");
+        obj3.setRa(1234);
+        obj3.setMedia(32);
+
+        System.out.println("RA: " + obj3.getRa());
     }
 }
--- Lista-2/src/main/java/br/edu/fatecfranca/exe0/Aluno.java
+++ Lista-2/src/main/java/br/edu/fatecfranca/exe0/Aluno.java
@@ -18,14 +18,42 @@ public class Aluno {
     }
 
     public void setRa(long ra) {
-        if (ra > 0){
+        String aux = String.valueOf(ra);
+        if (ra > 0 && aux.length() == 8){
             this.ra = ra;
+        }
+        else{
+            throw new IllegalArgumentException("Ra deve ser maior que zero e ter 8 digitos");
         }
     }
 
     public void setMedia(float media) {
-        if (media > 0){
+        if (media >= 0 && media <= 10){
             this.media = media;
         }
+        else{
+            throw new IllegalArgumentException("Media deve estar entre 0 e 10");
+        }
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public long getRa() {
+        return this.ra;
+    }
+
+    public float getMedia() {
+        return this.media;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", ra=" + ra +
+                ", media=" + media +
+                '}';
     }
 }
